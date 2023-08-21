271353fbe17d90b99a104d46bf143887338badec
甘楽 <74517119+subingjie@users.noreply.github.com>
2023-08-21 08:09:59 +0000

    add

--- time/time.go
+++ time/time.go
@@ -1,1 +1,12 @@
 package hitime
+
+import "time"
+
+func ParseStrTimeToInt(timeStr string) (int64, error) {
+	// 将timeStr转换成时间对象
+	timeObj, err := time.Parse("2006-01-02 15:04:05", timeStr)
+	if err != nil {
+		return 0, err
+	}
+	return timeObj.Unix(), nil
+}
